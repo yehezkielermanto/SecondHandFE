cb45fc76f7222aa299182261e2fd8edbb1e1cfdf
vitoradityafauzan <vitoraditya.f@gmail.com>
2022-06-16 20:04:16 +0700

    Masih Awalan

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// For Profile Page
+import Profile from './pages/Profile'
+// import NotFound from "./pages/404";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* Endpoint  for user profile */}
+      <Route path="/profile" element={<Profile />} />
+      {/* 404 */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  </BrowserRouter>
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import "../public/css/style.css";
+import ProfileForm from "../components/ProfileForm";
+import ProfileHeader from "../components/ProfileHeader"
+
+function Profile() {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default Profile;
