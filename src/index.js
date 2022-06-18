64ed9219fb81284ee9c1c50524e64db4b070f4bd
athallah aufar <athallahaufar741@gmail.com>
2022-06-18 17:57:52 +0700

    bug nggak bisa masuk regis

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import store from "./redux/store";
 
 import App from "./App";
 // For Profile Page
-import Profile from './pages/Profile';
-import Dashboard from './pages/Dashboard';
-import Register from './pages/Register';
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 // import NotFound from "./pages/404";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      {/* Endpoint  for user profile */}
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/register" element={<Register />} />
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        {/* Endpoint  for user profile */}
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/register"
+          element={
+            <GoogleOAuthProvider clientId="68980823363-57tmdid7nhefj7abt7l5u1jcbfdddg2p.apps.googleusercontent.com">
+              <Register />
+            </GoogleOAuthProvider>
+          }
+        />
 
-      {/* 404 */}
-      {/* <Route path="*" element={<NotFound />} /> */}
-    </Routes>
-  </BrowserRouter>
+        {/* 404 */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 
   // <React.StrictMode>
   //   <App />
