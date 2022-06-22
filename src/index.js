70c1f7cba024d01b75a8c59be0f705f92dc6d722
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2022-06-22 20:39:45 +0700

    update env

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 // import NotFound from "./pages/404";
 
-const { REACT_APP_CLIENT_ID } = process.env
+const { REACT_APP_ID } = process.env
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -31,7 +31,7 @@ root.render(
         <Route
           path="/login"
           element={
-            <GoogleOAuthProvider clientId={REACT_APP_CLIENT_ID}>
+            <GoogleOAuthProvider clientId={REACT_APP_ID}>
               <Login />
             </GoogleOAuthProvider>
           }
@@ -39,7 +39,7 @@ root.render(
         <Route
           path="/register"
           element={
-            <GoogleOAuthProvider clientId={REACT_APP_CLIENT_ID}>
+            <GoogleOAuthProvider clientId={REACT_APP_ID}>
               <Register />
             </GoogleOAuthProvider>
           }
