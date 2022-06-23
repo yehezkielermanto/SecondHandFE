dec46c73335b004e426f50518bb5ae1d9f72a59d
(Dalih) <dalih03praja@gail.com>
2022-06-23 15:28:18 +0700

    fixing

--- src/components/ButtonDashboard.jsx
+++ src/components/ButtonDashboard.jsx
@@ -7,7 +7,7 @@ const ButtonDashboard = () => {
       <>
       <div id="button">
       <h1 className='font-bold font-poppins mt-10'>Telusuri Kategori</h1>
-      <div className="flex w-full mt-1 snap-x">
+      <div className="flex w-full mt-1 snap-x bg-scroll">
         <div className="snap-center">
           <button
             className="flex font-poppins hover:bg-[#7126B5] bg-gray-300 px-6 py-2.5 hover:text-white text-gray-700 border border-solid border-gray-300 text-font-medium text-sm leading-tight rounded-[10px] shadow-md focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out mb-3 ml-1 mr-1"
