c7b4ff670d6928dc164bc56a0b46e4cde9fd73ad
Andien Khansa'a <khansaandien@gmail.com>
2022-06-23 21:29:47 +0700

    change dashboard page

--- src/components/HeaderProduct.jsx
+++ src/components/HeaderProduct.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom'
-import {FiArrowLeft, FiSearch, FiList, FiBell, FiUser} from 'react-icons/fi'
+import {FiMenu, FiSearch, FiList, FiBell, FiUser} from 'react-icons/fi'
 
 
-export default function Header(props) {
-    const navigate = useNavigate()
+export default function HeaderProduct() {
   return (
-    <nav className="sticky top-0  w-full flex flex-wrap items-center justify-between py-4 bg-white text-black 
-    shadow-none lg:shadow-md navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid w-full flex flex-wrap items-center justify-between lg:justify-around">
-            <div className="hidden flex-end lg:inline w-[5.88rem] h-8 bg-[#4B1979]"></div>
-                <button className="w-8 lg:hidden" onClick={() => navigate(-1)}>
-                    <FiArrowLeft className='inline mx-4 text-2xl' />
-                </button>
-				<div className="h-12 bg-white rounded-2xl py-3 px-6 text-neutral-3 flex lg:bg-[#EEEEEE]">
-					<input
-						className="w-full h-full bg-transparent"
-						placeholder="Cari di sini ..."
-					/>
-					<FiSearch className="text-2xl" />
-				</div>
-                <span className='text-base'></span>
-                <div className="hidden lg:flex">
-					<FiList className="xl:w-5 h-full mx-2" />
-					<FiBell className="xl:w-5 h-full mx-2" />
-					<FiUser className="xl:w-5 h-full mx-2" />
-				</div>
-       </div>    
-   </nav>
+    <div className="drop-shadow-lg pb-12">
+    {/* Header */}
+    <div className="w-full flex pt-8 px-4 gap-4 lg:bg-white lg:shadow-high lg:justify-between lg:py-4 lg:px-16">
+        <button
+            className="w-12 h-12 p-3 bg-white rounded-2xl lg:hidden"
+            onClick={() => setMobileSidebarOpen(true)}
+        >
+            <FiMenu className="w-full h-full" />
+        </button>
+
+        <div className="flex-grow lg:flex-grow-0 lg:flex lg:justify-center lg:items-center lg:gap-4">
+            <div className="hidden lg:inline w-[5.88rem] h-8 bg-[#7126B5]"></div>
+            <div className="h-12 bg-white rounded-2xl py-3 px-6 text-neutral-3 flex lg:bg-[#EEEEEE]">
+                <input
+                    className="w-full h-full bg-transparent"
+                    placeholder="Cari di sini ..."
+                />
+                <FiSearch className="text-2xl" />
+            </div>
+        </div>
+
+        <div className="hidden lg:flex">
+            <FiList className="xl:w-5 h-full mx-2" />
+            <FiBell className="xl:w-5 h-full mx-2" />
+            <FiUser className="xl:w-5 h-full mx-2" />
+        </div>
+    </div>
+</div>   
   )
 }
--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -24,34 +24,7 @@ export default function Product() {
 				close={() => setMobileSidebarOpen(false)}
 			/>
 			{/* Hero */}
-			<div className="drop-shadow-lg pb-12">
-				{/* Header */}
-				<div className="w-full flex pt-8 px-4 gap-4 lg:bg-white lg:shadow-high lg:justify-between lg:py-4 lg:px-16">
-					<button
-						className="w-12 h-12 p-3 bg-white rounded-2xl lg:hidden"
-						onClick={() => setMobileSidebarOpen(true)}
-					>
-						<FiMenu className="w-full h-full" />
-					</button>
-
-					<div className="flex-grow lg:flex-grow-0 lg:flex lg:justify-center lg:items-center lg:gap-4">
-						<div className="hidden lg:inline w-[5.88rem] h-8 bg-[#7126B5]"></div>
-						<div className="h-12 bg-white rounded-2xl py-3 px-6 text-neutral-3 flex lg:bg-[#EEEEEE]">
-							<input
-								className="w-full h-full bg-transparent"
-								placeholder="Cari di sini ..."
-							/>
-							<FiSearch className="text-2xl" />
-						</div>
-					</div>
-
-					<div className="hidden lg:flex">
-						<FiList className="xl:w-5 h-full mx-2" />
-						<FiBell className="xl:w-5 h-full mx-2" />
-						<FiUser className="xl:w-5 h-full mx-2" />
-					</div>
-				</div>
-			</div>	
+			
 			<HeaderProduct/>
 			<div className="md:flex items-start justify-center 2xl:px-20 md:px-6">
 				<div className="xl:w-2/5 lg:w-2/5 w-80 mb-20">
