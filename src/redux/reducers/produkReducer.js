317290a2ff7abed75841614e75a1bbe6fb1839f5
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2022-06-30 20:09:25 +0700

    update products

--- src/redux/reducers/produkReducer.js
+++ src/redux/reducers/produkReducer.js
@@ -1,30 +1,35 @@
-import { PRODUCT_ERROR, DELETE_PRODUCT } from "../actions/types";
+import { PRODUCT_ERROR, DELETE_PRODUCT, ADD_PRODUCT } from '../actions/types'
 const initialState = {
   product: [],
   detailProduct: [],
   editProduct: [],
   previewProduct: [],
-  status: [],
+  status: null,
   error: null,
-};
+}
 
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        status: 'success add product',
+      }
     case DELETE_PRODUCT:
       return {
         ...state,
         status: action.payload,
-      };
+      }
 
     case PRODUCT_ERROR:
       return {
         ...state,
         error: action.payload,
-        status: "FAIL",
-      };
+        status: 'FAIL',
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default productReducer;
+export default productReducer
