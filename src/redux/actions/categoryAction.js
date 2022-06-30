a8a1cea243c869df7b873c985e42e6ebc67018ec
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2022-06-30 18:53:59 +0700

    add template products

--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -1,24 +1,44 @@
-import { CATEGORY, CATEGORY_ERROR } from "./types"
-const {REACT_APP_URLENDPOINT} = process.env
+import { CATEGORY, CATEGORY_ERROR } from './types'
 
-export categoryError = (error) => async(dispatch)=>{
+export const categoryError = (error) => async (dispatch) => {
   dispatch({
     type: CATEGORY_ERROR,
-    payload: error.message
+    payload: error.message,
   })
 
-  setTimeout(()=>{
+  setTimeout(() => {
     dispatch({
       type: CATEGORY_ERROR,
-      payload: null
+      payload: null,
     })
   }, 5000)
 }
 
-export const listCategory = () => async (dispatch) =>{
+export const listCategory = () => async (dispatch) => {
   try {
-    const response = await fetch(`${process.env.REACT_APP_URLENDPOINT}/`)
+    const response = await fetch(
+      `${process.env.REACT_APP_URLENDPOINT}/api/v1/category`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+
+    const result = await response.json()
+
+    console.log(result)
+    if (result) {
+      dispatch({
+        type: CATEGORY,
+        payload: result.list,
+      })
+    }
   } catch (error) {
-    
+    dispatch({
+      type: CATEGORY_ERROR,
+      payload: error,
+    })
   }
 }
