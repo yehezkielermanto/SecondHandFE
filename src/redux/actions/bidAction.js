286bdc7eb842313bfbf1f63e117b5230b9ac94ab
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2022-07-22 22:01:41 +0700

    update env

--- src/redux/actions/bidAction.js
+++ src/redux/actions/bidAction.js
@@ -5,14 +5,14 @@ import {
   PRODUCT_ERROR,
   TRANSACTION_ERROR,
 } from './types'
-const { REACT_APP_ENDPOINT } = process.env
+// const { process.env.REACT_APP_ENDPOINT } = process.env
 
 export const createBid = (params) => async (dispatch) => {
   const { productId, sellerId } = params
   try {
     // masukkan ke tabel transaksi
     const response = await fetch(
-      REACT_APP_ENDPOINT +
+      process.env.REACT_APP_ENDPOINT +
         '/api/v1/transaction?' +
         new URLSearchParams({ sellerid: sellerId, productid: productId }),
       {
@@ -35,7 +35,7 @@ export const fetchTrans = () => async (dispatch) => {
   try {
     // ambil data dari transaksi
     const fetchTransaction = await fetch(
-      REACT_APP_ENDPOINT + '/api/v1/transaction',
+      process.env.REACT_APP_ENDPOINT + '/api/v1/transaction',
       { method: 'get', headers: { 'Content-Type': 'application/json' } },
     )
 
@@ -48,7 +48,7 @@ export const fetchTrans = () => async (dispatch) => {
     while (i < transaction.transaction.length) {
       // action komunikasi dengan backend untuk ambil detail gambar dari imagekit
       const fetchImgDetail = await fetch(
-        `${REACT_APP_ENDPOINT}/api/v1/products/picture/${transaction.transaction[i].barang.gambarbarangs[0].gambar}`,
+        `${process.env.REACT_APP_ENDPOINT}/api/v1/products/picture/${transaction.transaction[i].barang.gambarbarangs[0].gambar}`,
         { method: 'GET', headers: { 'Content-Type': 'application/json' } },
       )
       const imgDetail = await fetchImgDetail.json()
@@ -67,7 +67,7 @@ export const fetchTransId = (id) => async (dispatch) => {
   try {
     // console.log(id)
     const fetchTransaction = await fetch(
-      REACT_APP_ENDPOINT + '/api/v1/transaction/' + id,
+      process.env.REACT_APP_ENDPOINT + '/api/v1/transaction/' + id,
       { method: 'get', headers: { 'Content-Type': 'application/json' } },
     )
     const result = await fetchTransaction.json()
@@ -75,7 +75,7 @@ export const fetchTransId = (id) => async (dispatch) => {
 
     // action komunikasi dengan backend untuk ambil detail gambar dari imagekit
     const fetchImgDetail = await fetch(
-      `${REACT_APP_ENDPOINT}/api/v1/products/picture/${result.bid.bid.barang.gambarbarangs[0].gambar}`,
+      `${process.env.REACT_APP_ENDPOINT}/api/v1/products/picture/${result.bid.bid.barang.gambarbarangs[0].gambar}`,
       { method: 'GET', headers: { 'Content-Type': 'application/json' } },
     )
     const imgDetail = await fetchImgDetail.json()
@@ -107,7 +107,7 @@ export const DeniedBid = (id) => async (dispatch) => {
     if (id != '') {
       // console.log(id)
       const response = await fetch(
-        REACT_APP_ENDPOINT + '/api/v1/transaction/' + id,
+        process.env.REACT_APP_ENDPOINT + '/api/v1/transaction/' + id,
         {
           method: 'put',
           headers: {
@@ -134,7 +134,7 @@ export const AcceptBid = (id) => async (dispatch) => {
   try {
     if (id != '') {
       const response = await fetch(
-        REACT_APP_ENDPOINT + '/api/v1/transaction/product/' + id,
+        process.env.REACT_APP_ENDPOINT + '/api/v1/transaction/product/' + id,
         {
           method: 'put',
           headers: {
@@ -162,7 +162,7 @@ export const soldProduct = (id) => async (dispatch) => {
     if (id != '') {
       // console.log(id)
       const response = await fetch(
-        REACT_APP_ENDPOINT +
+        process.env.REACT_APP_ENDPOINT +
           `/api/v1/transaction/doneTrans/${id.idtrans}/${id.idbarang}`,
         {
           method: 'put',
@@ -190,7 +190,8 @@ export const declineTrans = (id) => async (dispatch) => {
   try {
     if (id != '') {
       const response = await fetch(
-        REACT_APP_ENDPOINT + `/api/v1/transaction/declineTrans/${id.idtrans}`,
+        process.env.REACT_APP_ENDPOINT +
+          `/api/v1/transaction/declineTrans/${id.idtrans}`,
         {
           method: 'put',
           headers: {
@@ -217,7 +218,7 @@ export const makeFalseStatus = (params) => async (dispatch) => {
   try {
     if (params != '') {
       const response = await fetch(
-        REACT_APP_ENDPOINT +
+        process.env.REACT_APP_ENDPOINT +
           `/api/v1/transaction/makeFalse/${params.iduser}/${params.idbarang}`,
         {
           method: 'put',
